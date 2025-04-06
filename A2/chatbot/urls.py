ed60c80805ed4e2d45687336c59c7529db648a2b
raix25 <raix9284@mylaurier.ca>
2025-04-06 18:00:39 -0400

    URL

--- A2/chatbot/urls.py
+++ A2/chatbot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # this is the homepage route
     path('chat/', views.chat, name='chat'),
 ]
